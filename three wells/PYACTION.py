a61b282159b7fde19b3d469fedfc21e17dc70eba
pp <6372815521@student.chula.ac.th>
2024-04-25 23:44:18 +0200

    Update PYACTION.py

--- three wells/PYACTION.py
+++ three wells/PYACTION.py
@@ -1,37 +1,55 @@
 import datetime
 
 def run(ecl_state, schedule, report_step, summary_state, actionx_callback):
-    if (not 'act01_executed' in globals()):
-        globals()["act01_executed"] = False
-        globals()["act02_executed"]= False
-        
+
     current_time = schedule.start + datetime.timedelta(seconds=summary_state.elapsed())
     
     BHP_TOP = summary_state.well_var("INJ","WBHP")
     BHP_BOT = summary_state.well_var("INJX","WBHP")
     DeltaP = BHP_BOT - BHP_TOP
     
-    if (DeltaP <= 0 and current_time.month >= 1 and current_time.year >= 2023 and not globals()["act01_executed"]):
-        print("Bottom layer close at {}\n".format(current_time))
-        kw = """
-        WCONINJE
-        'INJ' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	260	/
-	    'INJX' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	265 	/
-	    'I' 	    'WAT'	'SHUT'	RATE 	1000 	1* 	250	/
-        /
-        """
-    schedule.insert_keywords(kw)
-    globals()["act01_executed"] = True
+    #SHUT = no cross flow
+    #STOP = cross flow  
     
-    if (DeltaP > 0 and not globals()["act02_executed"]):
-        print("PYACTION reopens the cross-flow layer at {}\n".format(current_time))
-        kw = """
-        WCONINJE
-        'INJ' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	260 	/
-	    'INJX' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	265 	/
-	    'I' 	    'WAT'	'STOP'	RATE 	1000 	1* 	250	    /
-        /
-        """
-        schedule.insert_keywords(kw)
-        globals()["act02_executed"] = True
+    if (not DeltaP <= 0 ):
+        print("Bottom layer open at {}\n".format(current_time))
+        schedule.stop_well("I",report_step) 
+        
+    if (DeltaP <= 0 ):
+        print("Bottom layer close at {}\n".format(current_time))
+        schedule.shut_well("I",report_step+1)  
+    
+    
+    #if (DeltaP <= 0 ):
+    #    print("Bottom layer close at {}\n".format(current_time))
+    #    kw ="""
+    #    WELOPEN
+    #    I   OPEN                        /
+    #    I   OPEN    1   1   1           /
+    #    I   SHUT    1   1   3           /
+    #    /   
+    #    """
+    #    #'INJ' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	260	/
+	#    #'INJX' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	265 	/
+	#    #'I' 	    'WAT'	'SHUT'	RATE 	1000 	1* 	250	/
+    #    #/
+    #    #"""
+    #    schedule.insert_keywords(kw)
+    #
+    #if (not DeltaP <= 0): 
+    #    print("PYACTION reopens the cross-flow layer at {}\n".format(current_time))
+    #    kw = """
+    #    WELOPEN
+    #    I   OPEN                        /
+    #    I   OPEN    1   1   1           /
+    #    I   OPEN    1   1   3           /
+    #    /   
+    #    """
+    #    # WCONINJE
+    #    # 'INJ' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	260 	/
+	#    # 'INJX' 	    'WAT' 	'OPEN' 	BHP 	2000 	1* 	265 	/
+	#    # 'I' 	    'WAT'	'STOP'	RATE 	1000 	1* 	250	    /
+    #    # /
+    #    # """
+    #    schedule.insert_keywords(kw)
         
